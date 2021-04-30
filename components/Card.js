c439568eedf3d352260d67197417ff9a09ecf90a
Jhersy <jhersyvb@gmail.com>
2021-04-30 16:26:52 -0500

    03 - Fetching Data

---
+++ components/Card.js
@@ -0,0 +1,41 @@
+import styled from '@emotion/styled'
+
+function Card({ movie }) {
+  const { API_URL } = process.env
+
+  return (
+    <CardStyled>
+      <div className="poster">
+        <img src={API_URL + movie.poster.url} alt={movie.title} />
+      </div>
+      <div className="body">
+        <h3>{movie.title}</h3>
+        <p dangerouslySetInnerHTML={{ __html: movie.description }} />
+      </div>
+    </CardStyled>
+  )
+}
+
+const CardStyled = styled.div`
+  border: 1px solid #cccccc;
+  border-radius: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  margin-top: 50px;
+  overflow: hidden;
+  width: 400px;
+
+  .body {
+    padding: 20px;
+
+    h3 {
+      margin-bottom: 20px;
+    }
+
+    p {
+      color: #666666;
+      line-height: 1.5;
+    }
+  }
+`
+
+export default Card
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,28 @@
-export default function Home() {
+import Card from '../components/Card'
+
+const Home = ({ movies }) => {
+  console.log(movies)
+
   return (
     <div className="container">
-      <h1>This is our Front Page</h1>
+      {movies.map(movie => (
+        <Card key={movie.id} movie={movie} />
+      ))}
     </div>
   )
 }
+
+export async function getServerSideProps() {
+  const { API_URL } = process.env
+
+  const res = await fetch(`${API_URL}/movies`)
+  const data = await res.json()
+
+  return {
+    props: {
+      movies: data,
+    },
+  }
+}
+
+export default Home
