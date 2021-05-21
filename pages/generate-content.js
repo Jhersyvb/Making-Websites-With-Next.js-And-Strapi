ac71a6149f5022f016df25becde604c3e955b022
Jhersy <jhersyvb@gmail.com>
2021-05-20 22:25:45 -0500

    Generate Content With Faker.js

---
+++ pages/generate-content.js
@@ -0,0 +1,39 @@
+import getConfig from 'next/config'
+import { Box } from 'rebass'
+import faker from 'faker'
+
+const { publicRuntimeConfig } = getConfig()
+
+function GenerateContent() {
+  async function addContent() {
+    for (let i = 0; i < 10; i++) {
+      const postData = {
+        title: faker.lorem.sentence(),
+        content: faker.lorem.paragraphs(),
+      }
+
+      const generate = await fetch(`${publicRuntimeConfig.API_URL}/posts`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(postData),
+      })
+
+      const generateResponse = await generate.json()
+
+      console.log(generateResponse)
+    }
+  }
+
+  return (
+    <Box variant="container">
+      <button type="button" onClick={() => addContent()}>
+        Generate Strapi Post Content
+      </button>
+    </Box>
+  )
+}
+
+export default GenerateContent
